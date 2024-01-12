bda9d20dba900e06e33197190f5f8c8052b7afc8
imdiegodev1 <da.naranjo10@uniandes.edu.co>
2024-01-12 18:51:41 -0500

    application of single eresponsability principle

--- 1-SingleResponsability/Program.cs
+++ 1-SingleResponsability/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SingleResponsability;
 
 StudentRepository studentRepository = new();
-studentRepository.Export();
+ExportHelper exportHelpercsv = new();
+exportHelpercsv.ExportStudent(studentRepository.GetAll());
 Console.WriteLine("Proceso Completado");
